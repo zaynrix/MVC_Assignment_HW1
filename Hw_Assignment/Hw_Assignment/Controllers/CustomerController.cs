136a2bee45d1a58f9fb67ebe8da95e09b9443033
hp 15-bs <zayns1914@gmail.com>
2020-11-20 15:40:54 +0200

    import project

---
+++ Hw_Assignment/Hw_Assignment/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hw_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hw_Assignment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult List()
+        {
+            OurDatabase DB = new OurDatabase();
+            List<Customer> Customers;
+
+            Customers = DB.Customers;
+
+            return View(Customers);
+        }
+    }
+}
---
+++ Hw_Assignment/Hw_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hw_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
